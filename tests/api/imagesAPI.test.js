1156b1051e2badf9c856a1bbff64b02f65769883
Gwendolyn Villegas <gwen@Gwendolyns-MacBook-Pro.local>
2024-10-15 10:55:10 -0700

    updates based on feedback comments

--- tests/api/imagesAPI.test.js
+++ tests/api/imagesAPI.test.js
@@ -3,11 +3,11 @@ const Images  = require("../../models/ImagesAPI")
 describe('Test CAT API endpoints', () => {
     const images = new Images();
 
-    test('Search cat images', async() => {
+    test('Verify search all cat images endpoint return 200', async() => {
         const response = await images.searchImages();
         expect(response.status).toEqual(200);       
     })
-    test('Get a cat image', async() => {
+    test('Verify search a cat image endpoint return 200', async() => {
         const response = await images.searchImages();
         const imageID = response.data[0].id;    
         const responseGetImageAnalysis = await images.getImageAnalysis(imageID);
